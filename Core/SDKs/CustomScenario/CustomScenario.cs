72668dbfcdaed7579f7049a4d8cea18fa6974509
MakesYT <42534870+MakesYT@users.noreply.github.com>
2023-09-04 23:43:18 +0800

    布局触发器

---
+++ Core/SDKs/CustomScenario/CustomScenario.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Core.ViewModel.TaskEditor;
+
+namespace Core.SDKs.Services.Config;
+
+public partial class CustomScenario : ObservableRecipient
+{
+    /// <summary>
+    /// 手动执行
+    /// </summary>
+    [ObservableProperty] [NotifyPropertyChangedRecipients]
+    public bool executionManual = true;
+
+
+    [ObservableProperty] [NotifyPropertyChangedRecipients]
+    public List<string> keys;
+
+    /// <summary>
+    /// 间隔指定时间执行
+    /// </summary>
+    [ObservableProperty] [NotifyPropertyChangedRecipients]
+    public bool executionIntervalSpecifies = false;
+
+    [ObservableProperty] [NotifyPropertyChangedRecipients]
+    public List<TimeSpan> intervalSpecifiesTimeSpan;
+
+    /// <summary>
+    /// 指定时间执行
+    /// </summary>
+    [ObservableProperty] [NotifyPropertyChangedRecipients]
+    public bool executionScheduleTime = false;
+
+    [ObservableProperty] [NotifyPropertyChangedRecipients]
+    public TimeSpan scheduleTime;
+
+    public string? UUID
+    {
+        get;
+        set;
+    }
+
+    public BindingList<PointItem> nodes
+    {
+        get;
+        set;
+    } = new();
+
+    public BindingList<ConnectionItem> connections
+    {
+        get;
+        set;
+    } = new();
+
+    [ObservableProperty] [NotifyPropertyChangedRecipients]
+    public string _name = "任务";
+
+    [ObservableProperty] [NotifyPropertyChangedRecipients]
+    public string _description = "";
+}
---
+++ uToolkitopia/Controls/ResetIsEnabled.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Kitopia.Controls;
+
+public class ResetIsEnabled : ContentControl
+{
+    static ResetIsEnabled()
+    {
+        IsEnabledProperty.OverrideMetadata(
+            typeof(ResetIsEnabled),
+            new UIPropertyMetadata(
+                defaultValue: true,
+                propertyChangedCallback: (_, __) =>
+                {
+                },
+                coerceValueCallback: (_, x) => x));
+    }
+}
