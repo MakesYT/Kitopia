8b83c00a030875322e4870cb109de5ea367eb98b
MakesYT <42534870+MakesYT@users.noreply.github.com>
2023-09-06 23:35:12 +0800

    搜索框执行自定义情景,修复情景检测遇到判断时中断

--- Core/ViewModel/TaskEditor/TaskEditorViewModel.cs
+++ Core/ViewModel/TaskEditor/TaskEditorViewModel.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Core.SDKs.CustomScenario;
 using Core.SDKs.Services;
 using Core.SDKs.Services.Config;
 using Core.SDKs.Services.Plugin;
