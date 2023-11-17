3b8c8c922027764e3cdc080c9d52c370aedc07ea
MakesYT <42534870+MakesYT@users.noreply.github.com>
2023-11-17 09:45:23 +0800

    1

---
+++ uToolkitopia/View/Pages/Plugin/HotKeyManagerPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Kitopia.View.Pages.Plugin;
+
+/// <summary>
+/// HotKeyManagerPage.xaml 的交互逻辑
+/// </summary>
+public partial class HotKeyManagerPage : Page
+{
+    public HotKeyManagerPage()
+    {
+        InitializeComponent();
+    }
+}
